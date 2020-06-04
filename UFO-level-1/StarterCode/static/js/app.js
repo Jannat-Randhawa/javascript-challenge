664b7e77a9634b24319c397b86cda8b530d33de2
Jannat-Randhawa <58931852+Jannat-Randhawa@users.noreply.github.com>
2020-06-03 22:02:36 -0400

    date time setting

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -5,31 +5,53 @@ console.log(tableData);
 var tbody = d3.select("#ufo-table tbody");
 console.log(tbody);
 
-// var Date = d3.select("#Date").text();
-// var City = d3.select("#City").text();
-// var State = d3.select("#State").text();
-// var Country = d3.select("#Country").text();
-// var Shape = d3.select("#Shape").text();
-// var Duration = d3.select("#Duration").text();
-// var City = d3.select("#City").text();
+var Date = d3.select("#Date").text();
+var City = d3.select("#City").text();
+var State = d3.select("#State").text();
+var Country = d3.select("#Country").text();
+var Shape = d3.select("#Shape").text();
+var Duration = d3.select("#Duration").text();
+var City = d3.select("#City").text();
 
-// tableData.forEach(function(ufoSighting){
-//     console.log(ufoSighting);
-//     var row = tbody.append('tr');
-
-//     Object.entries(ufoSighting).forEach(function([key, value]){
-//         console.log(key, value);
-//         var cell = row.append('td');
-//         cell.text(value);
-//     });
-
-// });
-
-tableData.forEach((ufoSighting)=>{
+tableData.forEach(function(ufoSighting){
     console.log(ufoSighting);
     var row = tbody.append('tr');
 
-    Object.entries(ufoSighting).forEach(([key, value])=> {
-                var cell = row.append('td').text(value);
+    Object.entries(ufoSighting).forEach(function([key, value]){
+        console.log(key, value);
+        var cell = row.append('td');
+        cell.text(value);
     });
+
 });
+
+// tableData.forEach((ufoSighting)=>{
+//     console.log(ufoSighting);
+//     var row = tbody.append('tr');
+
+//     Object.entries(ufoSighting).forEach(([key, value])=> {
+//                 var cell = row.append('td').text(value);
+//     });
+// });
+
+var button = d3.select("#filter-btn")
+var form = d3.select("form-group")
+
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+function runEnter(){
+    d3.event.preventDefault();
+    var inputElement = d3.select("form-control");
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+    console.log(tableData);
+
+    var filteredDataValue = tableData.filter(tableData => tableData.datetime);
+    var outputData = inputValue === filteredDataValue
+    console.log(outputData)
+
+    var data = d3.select(".list-group");
+    data.html("");
+    data.append("li .filter").text(outputData);
+}
